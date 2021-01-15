3a79b6006efba201012c2a40aab1dcff8576c03d
Kendall Moraski <kendimoraski@gmail.com>
2021-01-15 15:29:19 -0500

    added ls file

--- bash.js
+++ bash.js
@@ -1,10 +1,7 @@
-process.stdout.write('prompt > ');
-process.stdin.on('data', (data) => {
-  const cmd = data.toString().trim();
-  if (cmd == 'pwd') {
-    process.stdout.write(`${process.cwd()}% `);
-  } else {
-    process.stdout.write('You typed: ' + cmd);
-    process.stdout.write('\nprompt > ');
-  }
-});
+const pwd = require('./pwd');
+
+pwd();
+
+const ls = require('./ls');
+
+ls();
---
+++ pwd.js
@@ -0,0 +1,24 @@
+process.stdout.write('prompt > ');
+
+process.stdin.on('data', data => {
+  const cmd = data.toString().trim();
+  if (cmd == 'pwd') {
+    process.stdout.write(`${process.cwd()}% `);
+  } else {
+    process.stdout.write('You typed: ' + cmd);
+    process.stdout.write('\nprompt > ');
+  }
+});
+
+module.exports = function () {
+  // pwd code
+  process.stdin.on('data', data => {
+    const cmd = data.toString().trim();
+    if (cmd == 'pwd') {
+      process.stdout.write(`${process.cwd()}% `);
+    } else {
+      process.stdout.write('You typed: ' + cmd);
+      process.stdout.write('\nprompt > ');
+    }
+  });
+};
